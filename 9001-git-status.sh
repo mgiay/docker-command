c6eeca6ad184e0849c789612bc1ef208537c1a32
Problem Solving On Unix/Linux Systems <35763628+mgiay@users.noreply.github.com>
2021-06-24 15:19:23 +0700

    Update 20210624-15h17
    Update 20210624-15h17

---
+++ 9001-git-status.sh
@@ -0,0 +1,10 @@
+##FILE_NAME="9001-git-status.sh"
+#LastUpdate: #8:55 2021.06.24
+##################################################
+#FILE_NAME="9001-git-status.sh"
+#FLD_LOC="/opt/docker-command"
+#cat $FLD_LOC/$FILE_NAME | grep LastUpdate
+##################################################
+echo "======================"
+echo "GITHUB STATUS:"
+git status
